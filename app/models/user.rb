857e6b8d09cf7029aa889ca87f430bdd168b21d8
= <=>
2022-09-20 17:33:54 +0530

    Create User Mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_secure_token :verification_token
+  
+  attr_accessor :password_digest
+
   validates :name, :email, presence: true
   validates :email, format: { with: EMAIL_REGEX }
 end
