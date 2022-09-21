58838cf7aae073f2a31a531d07fdd0414cdf058d
= <=>
2022-09-21 16:49:44 +0530

    Add attribute verification_token to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_102603) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_165100) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_102603) do
     t.datetime "verified_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "verification_token"
   end
 
 end
