051d8ceee44cd90fa5184902836893c886e9a5f2
= <=>
2022-09-21 18:22:34 +0530

    Add login and logout logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    user = User.find_by(email: permitted_params[:email])
+
+    if user.try(:authenticate, permitted_params[:password])
+      session[:user_id] = user.id
+      # redirect_to deals_path
+    else
+      # redirect_to login_url
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    # redirect_to login_url
+  end
+
+  private
+    def permitted_params
+      params.require(:user).permit(:email, :password)
+    end
 end
