f402768770d2865977b3584fb3e8fc5c463c4854
= <=>
2022-09-21 17:44:53 +0530

    Add password validations and remove default validations of has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password(attribute = :password, validations: false)
+  has_secure_password validations: false
   has_secure_token :verification_token
   
   attr_accessor :password_digest
 
   validates :name, :email, presence: true
+  validates :password, confirmation: true, allow_blank: true
+  validates :password, :password_confirmation, presence: true, if: :setting_password?
   validates :email, format: { with: EMAIL_REGEX }
 
   after_create_commit :send_verification_email
@@ -12,5 +14,9 @@ class User < ApplicationRecord
   private
     def send_verification_email
       UserMailer.verify_email(self).deliver_now
+    end
+
+    def setting_password?
+      password || password_confirmation
     end
 end
