3d7daa44e70a1b2c420488179108e6f890979bbc
= <=>
2022-09-22 15:28:18 +0530

    Restrict application access to logged in user only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
+  private
+    def authorize
+      redirect_to login_url, notice: 'Please login to access the app' if cookies[:user_id].blank?
+    end
 end
