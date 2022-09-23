4ae3837eae4bd069a29627f6a78b9d0d67397657
= <=>
2022-09-23 10:56:08 +0530

    Remember user based on their choice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: permitted_params[:email])
+    cookie_expiration = 10.days.from_now if permitted_params[:remember_me] == '1'
+    p permitted_params
 
     if user.try(:authenticate, permitted_params[:password])
       if user.verified_at.present?
         cookies.encrypted[:user_id] = {
           value: user.id,
-          expires: 10.days.from_now
+          expires: cookie_expiration
         }
         redirect_to deals_path
       else
@@ -29,6 +31,6 @@ class SessionsController < ApplicationController
 
   private
     def permitted_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:email, :password, :remember_me)
     end
 end
