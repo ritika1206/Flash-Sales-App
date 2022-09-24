37dcdf1ef2f53678840bdcb28af4ae50e4fd4837
= <=>
2022-09-24 11:36:55 +0530

    Add view for deals page

--- app/controllers/admin/deals_controller.rb
+++ app/controllers/admin/deals_controller.rb
@@ -1,10 +1,27 @@
 class Admin::DealsController < Admin::BaseController
   def index
+    @deals = Deal.all
   end
 
   def new
+    @deal = Deal.new
   end
 
   def create
+    p params
+    p deal_permitted_params
+    deal = Deal.new(deal_permitted_params)
+    deal.images.attach params[:deal][:images]
+
+    if deal.save!
+      redirect_to admin_deals_url
+    else
+      redirect_to new_admin_deal_url, notice: 'Something went wrong'
+    end
   end
+
+  private
+    def deal_permitted_params
+      params.require(:deal).permit(:title, :description, :price_in_cents, :discount_price_in_cents, :quantity, :tax_percentage, :published_at)
+    end
 end
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,6 @@
 class Deal < ApplicationRecord
+  has_many_attached :images
+
   validates :title, :description, :price_in_cents, :discount_price_in_cents, :quantity, :tax_percentage, :published_at, presence: true 
   validates :title, uniqueness: true
   validates :price_in_cents, :discount_price_in_cents, :quantity, :tax_percentage, numericality: true
