24153caf9758d57052aa917dc592a19d05bcf647
= <=>
2022-09-26 18:16:36 +0530

    Extract cookie expiration duration in constant

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,4 @@
-EMAIL_REGEX = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
+namespace :user do
+  EMAIL_REGEX = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
+  COOKIE_EXPIRATION_DURATION = 10.days
+end
