7e582ade2ea9c86e6e5440eb0e4a46c722bcdb00
= <=>
2022-09-26 17:46:37 +0530

    Handle purging images on deal destruction

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,19 +1,29 @@
 class Deal < ApplicationRecord
   has_many_attached :images
-  belongs_to :admin, class_name: "User"
-  after_save :publishable?, unless: ->{ is_publishable }
+  belongs_to :admin, class_name: "User", optional: true
+
+  before_update :restrict_deletion_or_publish_date_updation
+  before_destroy :restrict_deletion_or_publish_date_updation
+  after_destroy { |deal| deal.images.purge }
+  after_commit :publishable?, unless: ->{ is_publishable }
 
   validates :title, :description, :price_in_cents, :discount_price_in_cents, :quantity, :tax_percentage, :published_at, presence: true 
   validates :title, uniqueness: true
   validates :price_in_cents, :discount_price_in_cents, :quantity, :tax_percentage, numericality: true
   scope :number_of_deals_with_publish_date, ->(date) { where(published_at: date).size }
 
-  private
-    def publishable?
-      if images.size >= 2 && quantity > 10 && number_of_deals_with_publish_date(date) <= 2
-        update!(is_publishable: true)
-      else
-        update!(is_publishable: false)
-      end
-    end
+  def publishable?
+    return true if images.size >= 2 && quantity > 10 && Deal.number_of_deals_with_publish_date(published_at) <= 2
+
+  end
+
+  def less_than_one_day_away_from_publish?
+    p published_at
+    (published_at - Date.today).to_i <= 1
+  end
+
+  def restrict_deletion_or_publish_date_updation
+    p self.less_than_one_day_away_from_publish?
+    throw :abort if self.less_than_one_day_away_from_publish?
+  end
 end
