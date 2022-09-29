cdf4d3cdec021798ccc1678cb841e886fbf7d28e
= <=>
2022-09-29 13:06:37 +0530

    Some change in layout

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,9 +1,9 @@
 class DealsController < ApplicationController
   def index
-    @live_deals = Deal.live_deals
+    @deals = Deal.live_deals
   end
 
   def show
-    @live_deal = Deal.find(params[:id])
+    @deal = Deal.find(params[:id])
   end
 end
