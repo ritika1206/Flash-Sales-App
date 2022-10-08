e344886eb488c0e084314a3b1e419f87bbf04a9f
= <=>
2022-10-08 12:07:14 +0530

    Change in line item destroy logic

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
   def destroy
     line_item = LineItem.find(params[:id])
-    if line_item.destroy
+    if line_item.order.line_items.destroy(line_item)
       notice = t(:successfull, resource_name: 'deleted deal from order')
     else
       notice = t(:default_error_message)
