bd82bda31e865e16929ada999a7ac2d83bdf0be0
= <=>
2022-10-08 11:59:41 +0530

    Create method to calculate discount and loyality discount price

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,23 +2,11 @@ class LineItem < ApplicationRecord
   belongs_to :order
   belongs_to :deal
 
-  def calculate_loyality_discount_percentage
-    user_order_number = Order.count(email: line_item.order.user.email)
-    case
-    when user_order_number >= 6
-      loyality_discount_percentage = 5
-    when user_order_number = 5
-      loyality_discount_percentage = 4
-    when user_order_number = 4
-      loyality_discount_percentage = 3
-    when user_order_number = 3
-      loyality_discount_percentage = 2
-    when user_order_number = 2
-      loyality_discount_percentage = 1
-    end
-  end
+  before_create :calculate_loyality_discount_percentage_and_price
 
-  def calculate_loyality_discount_price
-    loyality_discount_price = discount_price*((100-loyality_discount_percentage)/100)
+  def calculate_loyality_discount_percentage_and_price
+    user_order_number = Order.where(user_id: order.user_id).count
+    self.loyality_discount_percentage = [5, user_order_number - 1].min
+    self.loyality_discounted_price = self.discounted_price * ((100 - self.loyality_discount_percentage) / 100.0)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
-  has_many :line_items, dependent: :destroy
+  has_many :line_items, dependent: :destroy, after_add: :calculate_price_and_discount_price_on_add, after_remove: :calculate_price_and_discount_price_on_remove, before_add: :prevent_multiple_purchase_of_same_deal, autosave: true
   belongs_to :user
+  has_many :order_transactions, dependent: :destroy
+  has_many :deals, through: :line_items
 
   enum status: { 
     in_cart: 'in_cart',
@@ -13,11 +15,33 @@ class Order < ApplicationRecord
 
   # validates :discount_price_in_cents, :loyality_discounted_price, numericality: true, allow_blank: true
 
-  # def calculate_price
-  #   line_items.each { |line_item| price += line_item.discount_price }
-  # end
+  def calculate_price_and_discount_price_on_add(line_item)
+    self.price += line_item.discounted_price
+    self.discount_price += line_item.loyality_discounted_price
+    self.save
 
-  # def calculate_discount_price
-  #   line_items.each { |line_item| price += line_item.loyality_discount_price }
-  # end
+    line_item.deal.quantity -= 1
+    line_item.deal.save
+  end
+
+  def calculate_price_and_discount_price_on_remove(line_item)
+    self.price -= line_item.discounted_price
+    self.discount_price -= line_item.loyality_discounted_price
+    if self.price == 0
+      self.destroy
+    else
+      self.save
+    end
+
+    line_item.deal.quantity += 1
+    line_item.deal.save
+  end
+
+  def prevent_multiple_purchase_of_same_deal(line_item)
+    self.user.orders.each do |order|
+      order.line_items.each do |user_line_item|
+        throw :abort if user_line_item.deal_id == line_item.deal_id
+      end
+    end
+  end
 end
