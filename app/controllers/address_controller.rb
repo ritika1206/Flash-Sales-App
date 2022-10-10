7e8a99eb17c1d3576efcda85e346db9a09b3e4df
= <=>
2022-10-10 11:19:28 +0530

    Handling shipping address selection by the user and add permitted params helper method

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,10 +1,13 @@
 class AddressController < ApplicationController
   def create
     user = User.find_by(verification_token: params[:verification_token])
-
     address = user.addresses.build(permitted_address_params)
+
     if address.save
-      redirect_to order_url(logged_in_user.orders.find_by(status: 'in_cart'), notice: t(:successfull, resource_name: 'added shipping address for order')
+      order = logged_in_user_cart_order
+      order.shipping_address_id = address.id
+      order.save
+      redirect_to order_url(logged_in_user_cart_order), notice: t(:successfull, resource_name: 'added shipping address for order')
     else
       redirect_to new_address_url, notice: t(:default_error_message)
     end
@@ -13,4 +16,18 @@ class AddressController < ApplicationController
   def new
     @address = Address.new
   end
+
+  def shipping
+    if logged_in_user_cart_order.update(shipping_address_id: permitted_address_params[:shipping_address_id])
+      redirect_to order_url(logged_in_user_cart_order), notice: t(:successfull, resource_name: 'added shipping address for order')
+    else
+      redirect_to new_address_url, notice: t(:default_error_message)
+    end
+  end
+
+  private
+
+    def permitted_address_params
+      params.require(:address).permit(:country, :city, :state, :line1, :line2, :postal_code, :shipping_address_id)
+    end
 end
