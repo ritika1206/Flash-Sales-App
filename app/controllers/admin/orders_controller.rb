6141f7127a3849dfac290d5509d315f2e65877df
= <=>
2022-10-10 11:21:35 +0530

    Initialize users instance variable with all app users

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,5 +2,6 @@ class Admin::OrdersController < Admin::BaseController
   def index
     @orders = Order.all
     @orders = User.find_by(email: params[:email]).orders if params[:email].present?
+    @users = User.all
   end
 end
