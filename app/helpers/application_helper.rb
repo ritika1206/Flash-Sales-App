375dac30998822102a6824c767a0b05c5436d51e
= <=>
2022-10-10 11:09:41 +0530

    Add method to get the order with status as in cart of logged in user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,13 @@ module ApplicationHelper
     return true if deals.first.live?
 
   end
+
+  def logged_in_user_last_used_shipping_address
+    last_used_address = Address.find_by(id: logged_in_user.orders.last.shipping_address_id)
+    last_used_address.present? ? last_used_address : logged_in_user.addresses.last
+  end
+
+  def logged_in_user_cart_order
+    logged_in_user.orders.find_by(status: 'in_cart')
+  end
 end
