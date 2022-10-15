0d36295a739a18fb2c9b2ed5850d3f9083f8e011
= <=>
2022-10-15 11:38:56 +0530

    Add @default_end_date instance variable

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -9,6 +9,7 @@ class Admin::ReportsController < Admin::BaseController
       p @customers_expenditure = User.customer_expenditure(params[:from].to_date, params[:to].to_date + 1)
     else
       @default_start_date = FlashSales::Reports::DEFAULT_CUSTOMER_EXPENDITIURE_START_DATE
+      @default_end_date = Date.today
       @customers_expenditure = User.customer_expenditure(@default_start_date, Date.today + 1)
     end
   end
