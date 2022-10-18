10265ed824fb685a1749f1c5fe4ee010d9b90a07
= <=>
2022-10-18 13:11:08 +0530

    Extract finding order transaction for the order in params in before action

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,6 @@
 class Admin::OrdersController < Admin::BaseController
+  before_action :order_transaction_for_order_in_params
+
   def index
     @orders = Order.all
     @users = User.all
@@ -9,10 +11,10 @@ class Admin::OrdersController < Admin::BaseController
   end
 
   def mark_status
-    order_transaction = OrderTransaction.find_by(order_id: params[:order_id])
+    @order_transaction = OrderTransaction.find_by(order_id: params[:order_id])
     order = Order.find(params[:order_id])
 
-    if order_transaction.paid?
+    if @order_transaction.paid?
       if permitted_params[:status] == 'In Cart'
         redirect_to order_url(id: params[:order_id]), alert: 'Update not allowed'
       else
@@ -28,5 +30,10 @@ class Admin::OrdersController < Admin::BaseController
 
     def permitted_params
       params.require(:order).permit(:status)
+    end
+
+    def order_transaction_for_order_in_params
+      @order_transaction = OrderTransaction.find_by(order_id: params[:order_id])
+      redirect_to admin_orders_url, alert: 'No transaction exist for the order' if @order_transaction.blank?
     end
 end
