0a4e8efd490172a208da8bffa2ad7ccfa15edf96
= <=>
2022-10-18 12:08:40 +0530

    Make status enum and send order placement mail only when transaction is paid

--- app/models/order_transaction.rb
+++ app/models/order_transaction.rb
@@ -1,8 +1,10 @@
 class OrderTransaction < ApplicationRecord
   belongs_to :order
   belongs_to :address, foreign_key: 'shipping_address_id'
+
+  enum status: [ :unpaid, :refunded, :paid ]
   
-  after_create_commit :send_placement_confirmation_mail
+  after_create_commit :send_placement_confirmation_mail, if: ->(transaction) { transaction.paid? }
 
   def send_placement_confirmation_mail
     OrderTransactionMailer.order_placement_confirmation(self).deliver_now
