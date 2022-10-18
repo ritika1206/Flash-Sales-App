ee60793c25babacc243bc94a24a772134062ec7a
= <=>
2022-10-18 12:06:03 +0530

    Add validations

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,5 +1,8 @@
 class Deal < ApplicationRecord
-  include ActiveModel::Dirty
+  DESCRIPTION_REGEX = /\A(?:\b\w+\b[\s\r\n.,'"?!]*){1,200}\z/
+  TITLE_REGEX = /\A(?:\b\w+\b[\s\r\n.,'"?!]*){1,15}\z/
+  MIN_INITIAL_QUANTITY = 10
+  TAX_PERCENTAGE_RANGE = 0..28
 
   PUBLISHABLE_DEAL_IMAGE_QUANTITY = 2
   PUBLISHABLE_DEAL_QUANTITY = 10
@@ -11,14 +14,21 @@ class Deal < ApplicationRecord
   has_many :orders, through: :line_items, dependent: :destroy
   belongs_to :admin, class_name: "User", foreign_key: 'created_by'
 
-  before_update :restrict_updation, :set_current_quantity
-  before_destroy :restrict_deletion
+  before_update :restrict, if: ->(deal) { deal.less_than_one_day_away_from_publish? && (deal.status_change.try(:last) == 'deleted' || deal.published_at_changed?) }
+  before_update :set_current_quantity
   after_destroy { |deal| deal.images.purge }
   after_commit :publishable?, unless: ->{ is_publishable }
 
-  validates :title, :description, :price_in_cents, :discount_price_in_cents, :initial_quantity, :tax_percentage, :published_at, presence: true 
-  validates :title, uniqueness: true
-  validates :price_in_cents, :discount_price_in_cents, :initial_quantity, :tax_percentage, numericality: true
+  validates :title, :description, :price_in_cents, :discount_price_in_cents, :initial_quantity, :tax_percentage, presence: true
+  validates :description, format: { with: DESCRIPTION_REGEX, message: 'can contain maximum 200 words' }, allow_blank: true
+  validates :title, format: { with: TITLE_REGEX, message: 'can contain maximum 15 words' }, uniqueness: { case_sensitive: false }, allow_blank: true
+  validates :tax_percentage, numericality: { in: TAX_PERCENTAGE_RANGE }, allow_blank: true
+  validates :price_in_cents, numericality: { greater_than_or_equal_to: 0, message: 'should be a positive value' }, allow_blank: true
+  validates :discount_price_in_cents, numericality: { less_than_or_equal_to: :price_in_cents }, if: [:price_in_cents?, :discount_price_in_cents?, ->(deal) { deal.price_in_cents > 0.1 }]
+  validates :discount_price_in_cents, numericality: { greater_than_or_equal_to: 0, message: 'should be a positive value' }, allow_blank: true
+  validates :initial_quantity, numericality: { greater_than_or_equal_to: MIN_INITIAL_QUANTITY }, allow_blank: true, on: :create
+  validates :current_quantity, numericality: { less_than_or_equal_to: :initial_quantity }, allow_blank: true
+  validates :published_at, comparison: { greater_than_or_equal_to: Date.today}, on: :create
 
   scope :number_of_deals_with_publish_date, ->(date) { where(published_at: date).size }
   scope :live_deals, -> { where(status: 'live') }
@@ -26,10 +36,10 @@ class Deal < ApplicationRecord
   scope :published_deals, -> { where(status: 'published') }
   scope :deals_revenue, -> { published_deals.select(:id, :title, '(initial_quantity - current_quantity) * discount_price_in_cents / 100 AS revenue').order(revenue: :desc) }
 
-  enum status: { live: 'live', unpublished: 'unpublished', published: 'published' }
+  enum status: { live: 'live', unpublished: 'unpublished', published: 'published', deleted: 'deleted'}
 
   def publishable?
-    return true if images.size >= PUBLISHABLE_DEAL_IMAGE_QUANTITY && initial_quantity > PUBLISHABLE_DEAL_QUANTITY && Deal.number_of_deals_with_publish_date(published_at) <= DEAL_WITH_SAME_PUBLISH_DATE_QUANTITY && !published?
+    return true if images.size >= PUBLISHABLE_DEAL_IMAGE_QUANTITY && initial_quantity > PUBLISHABLE_DEAL_QUANTITY && Deal.number_of_deals_with_publish_date(published_at) <= DEAL_WITH_SAME_PUBLISH_DATE_QUANTITY && !published? && !deleted?
 
   end
 
@@ -37,12 +47,8 @@ class Deal < ApplicationRecord
     (published_at - Date.today).to_i <= DATE_DIFFERENCE && (published_at - Date.today).to_i >= 0
   end
 
-  def restrict_deletion
-    throw :abort if less_than_one_day_away_from_publish?
-  end
-
-  def restrict_updation
-    throw :abort if less_than_one_day_away_from_publish? && published_at_changed?
+  def restrict
+    throw :abort
   end
 
   def set_current_quantity
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  NAME_REGEX = /\A[A-Z]+\z/i
+  EMAIL_REGEX = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
+  MIN_PASSWORD_LENGTH = 6
+  MIN_NAME_LENGTH = 3
+
   has_secure_password
   has_secure_token :verification_token
   has_secure_token :api_token
@@ -10,9 +15,11 @@ class User < ApplicationRecord
   has_many :line_items, through: :orders
 
   validates :name, :email, presence: true
-  validates :password, confirmation: true, allow_blank: true
+  validates :password, confirmation: true
+  validates :password, length: { minimum: MIN_PASSWORD_LENGTH }, allow_blank: true
   validates :password, :password_confirmation, presence: true, if: :setting_password?
-  validates :email, format: { with: FlashSales::User::EMAIL_REGEX }, uniqueness: true
+  validates :email, format: { with: EMAIL_REGEX, message: 'invalid format' }, uniqueness: { case_sensitive: false }, allow_blank: true
+  validates :name, format: { with: NAME_REGEX, message: 'can only contain alphabets' }, length: { minimum: MIN_NAME_LENGTH }, allow_blank: true
 
   after_create_commit :send_verification_email, unless: :verified_at?
 
@@ -26,6 +33,6 @@ class User < ApplicationRecord
     end
 
     def setting_password?
-      password || password_confirmation
+      password_digest_changed?
     end
 end
